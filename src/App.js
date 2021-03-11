254b22b096de0faf26d05dc7c93549aeaf85194c
Sai-Tejaswi <chisate1998@gmail.com>
2021-03-11 18:24:11 +0530

    mhey there

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+
+import './App.css';
+import HomePage from './homepage.comp';
+function App() {
+  return <div>
+    <HomePage />
+    </div>
+    
+}
+
+export default App;
